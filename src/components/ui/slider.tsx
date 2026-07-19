bb9504e90b32727690e89a196f5c6e420cde752a
Felype <felyped03@gmail.com>
2026-07-18 22:05:31 -0300

    Update slider.tsx

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -3,21 +3,61 @@ import * as SliderPrimitive from "@radix-ui/react-slider";
 
 import { cn } from "@/lib/utils";
 
-const Slider = React.forwardRef<
+export interface SliderProps
+  extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> {}
+
+const sliderRootClassName = [
+  "relative",
+  "flex w-full items-center",
+  "touch-none select-none",
+].join(" ");
+
+const sliderTrackClassName = [
+  "relative",
+  "h-1.5 w-full grow",
+  "overflow-hidden rounded-full",
+  "bg-primary/20",
+].join(" ");
+
+const sliderRangeClassName = [
+  "absolute",
+  "h-full",
+  "bg-primary",
+].join(" ");
+
+const sliderThumbClassName = [
+  "block h-4 w-4",
+  "rounded-full",
+  "border border-primary/50",
+  "bg-background",
+  "shadow-sm",
+  "transition-colors",
+
+  "focus-visible:outline-none",
+  "focus-visible:ring-2",
+  "focus-visible:ring-ring",
+  "focus-visible:ring-offset-2",
+  "focus-visible:ring-offset-background",
+
+  "disabled:pointer-events-none",
+  "disabled:opacity-50",
+].join(" ");
+
+export const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+  SliderProps
 >(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
-    className={cn("relative flex w-full touch-none select-none items-center", className)}
+    className={cn(sliderRootClassName, className)}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    <SliderPrimitive.Track className={sliderTrackClassName}>
+      <SliderPrimitive.Range className={sliderRangeClassName} />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+
+    <SliderPrimitive.Thumb className={sliderThumbClassName} />
   </SliderPrimitive.Root>
 ));
-Slider.displayName = SliderPrimitive.Root.displayName;
 
-export { Slider };
+Slider.displayName = "Slider";
